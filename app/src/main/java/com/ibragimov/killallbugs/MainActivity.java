3d2418b7d6916578b231b3eb1e8c69f20bfc8917
darkdubdeep <darkdubdeep@gmail.com>
2022-02-19 12:28:34 +0100

    Develop settings

--- app/src/main/java/com/ibragimov/killallbugs/MainActivity.java
+++ app/src/main/java/com/ibragimov/killallbugs/MainActivity.java
@@ -2,8 +2,14 @@ package com.ibragimov.killallbugs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
         int recordScore;
         recordScore=getResources().getInteger(R.integer.record);
         recordTextView.setText(String.valueOf(recordScore));
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // save player name to settings
+        TextInputLayout nameTextInput = (TextInputLayout)findViewById(R.id.nameInput);
+        String playerName = String.valueOf(Objects.requireNonNull(nameTextInput.getEditText()).getText());
+        SharedPreferences settings=getPreferences(0);
+        SharedPreferences.Editor editor=settings.edit();
+        editor.putString("playerName",playerName);
+
+        // save bugs count to settings
+        EditText bugsCountInput = (EditText)findViewById(R.id.editTextNumber);
+        int bugsCoung = Integer.parseInt(String.valueOf(bugsCountInput.getText()));
+        editor.putInt("bugsCount",bugsCoung);
+        editor.apply();
     }
 }
