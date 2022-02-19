1ab98a95f96a142e6bc099df8b1122a459290d3f
darkdubdeep <darkdubdeep@gmail.com>
2022-02-19 11:51:47 +0100

    Develop settings

--- app/src/main/java/com/ibragimov/killallbugs/MainActivity.java
+++ app/src/main/java/com/ibragimov/killallbugs/MainActivity.java
@@ -3,6 +3,7 @@ package com.ibragimov.killallbugs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView recordTextView = (TextView)findViewById(R.id.recordLabelValue);
+        int recordScore;
+        recordScore=getResources().getInteger(R.integer.record);
+        recordTextView.setText(String.valueOf(recordScore));
     }
 }
